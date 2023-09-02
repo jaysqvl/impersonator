e45564924e519c5d7da27a9f2ab8e0540909436e
Jays-PC <jaysqvl@gmail.com>
2023-09-02 01:47:34 -0700

    Refactor to support global settings switching

--- src/script.py
+++ src/script.py
@@ -18,9 +18,9 @@ import pyautogui
 # Loads the .env file
 load_dotenv()
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Global Dynamic APP Settings
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 change_default_llm = False
 change_default_vdb = False
 llm = "OpenAI"
@@ -36,67 +36,51 @@ pc_api_key = None
 supabase: Client = create_client(sb_proj_url, sb_api_key)
 pinecone.init(api_key=os.getenv('PINECONE_API_KEY'), environment="us-west1-gcp")
 
-def generate_answer(user_input):
-    response = st.session_state.conversation({'question': user_input})
-    st.session_state.chat_history = response['chat_history']
 
-    for i, message in enumerate(st.session_state.chat_history):
-        if i % 2 == 0:
-            st_message(message.content, is_user=True)
-        else:
-            st_message(message.content, is_user=False)
-    
-    # Clears the input text
-    # st.session_state["input_text"] = ""
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Global Reset Functions
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-def ingest(pdfs):
-    text = get_text(pdfs)
-    text_batches = get_text_batches(text)
+def reset_app_hard():
+    pyautogui.hotkey("ctrl", "F5")
 
-    return text_batches
+def reset_settings():
+    # Resets all the global APP settings
+    global llm; llm_model; vector_db; temp_key; oa_api_key; sb_api_key; sb_proj_url; hf_api_key; pc_api_key
 
-def get_text(pdfs):
-    text = ""
-    for pdf in pdfs:
-        pdf_reader = PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text += page.extract_text()
-    return text
+    llm = "OpenAI"
+    llm_model = "gpt-3.5-turbo"
+    vector_db = "Supabase"
+    temp_key = ""
+    oa_api_key = os.getenv('OPENAI_API_KEY')
+    sb_api_key = os.getenv('SUPABASE_API_KEY')
+    sb_proj_url = os.getenv('SUPABASE_PROJ_URL')
+    hf_api_key = None
+    pc_api_key = None
 
-def get_text_batches(text):
-    text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size = 1000,
-        chunk_overlap = 100
-    )
-    return text_splitter.split_text(text)
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Settings Selection Section
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# Creates Vector DB
-def init_vector_db(text_batches):
-    print(text_batches)
-    embeddings = OpenAIEmbeddings(openai_api_key=oa_api_key)
-    vector_db = SupabaseVectorStore.from_texts(text_batches, embeddings, client=supabase, table_name="documents")
-    
-    # for switching to pinecone
-    # vecter_db = pinecone.create_index("python-index", dimension=1536, metric="cosine")
+def change_openai_api_key(api_key):
+    global oa_api_key
+    os.environ['OPENAI_API_KEY'] = api_key
+    oa_api_key = api_key
 
-    return vector_db
+def change_supabase_api_key(api_key):
+    global sb_api_key
+    os.environ['SUPABASE_API_KEY'] = api_key
+    sb_api_key = api_key
 
-def get_conversation_chain(vector_db):
-    if llm == "OpenAI":
-        llm = ChatOpenAI(openai_api_key=oa_api_key, model=llm_model)
-    elif llm == "HuggingFace":
-        llm = HuggingFaceHub(repo_id=llm_model, model_kwargs={"temperature":0.5, "max_length":512})
+def change_huggingface_api_key(api_key):
+    # No default key
+    global hf_api_key
+    hf_api_key = api_key
 
-    memory = ConversationBufferMemory(
-        memory_key='chat_history', return_messages=True)
-    conversation_chain = ConversationalRetrievalChain.from_llm(
-        llm=llm,
-        retriever=vector_db.as_retriever(search_type="similarity"),
-        memory=memory
-    )
-
-    return conversation_chain
+def change_pinecone_api_key(api_key):
+    # No default key
+    global pc_api_key
+    pc_api_key = api_key
 
 # Checks the validity of the OpenAI API Key before applying it
 def check_openai_api_key(api_key):
@@ -123,51 +107,6 @@ def check_openai_api_key(api_key):
         print(f"An error occurred: {response.status_code}, {response.json()}")
         return False
 
-def reset_app_hard():
-    pyautogui.hotkey("ctrl", "F5")
-
-def reset_settings():
-    # Resets all the global APP settings
-    global llm; llm_model; vector_db; temp_key; oa_api_key; sb_api_key; sb_proj_url; hf_api_key; pc_api_key
-
-    llm = "OpenAI"
-    llm_model = "gpt-3.5-turbo"
-    vector_db = "Supabase"
-    temp_key = ""
-    oa_api_key = os.getenv('OPENAI_API_KEY')
-    sb_api_key = os.getenv('SUPABASE_API_KEY')
-    sb_proj_url = os.getenv('SUPABASE_PROJ_URL')
-    hf_api_key = None
-    pc_api_key = None
-
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Setters for the API Keys
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-def change_openai_api_key(api_key):
-    global oa_api_key
-    os.environ['OPENAI_API_KEY'] = api_key
-    oa_api_key = api_key
-
-def change_supabase_api_key(api_key):
-    global sb_api_key
-    os.environ['SUPABASE_API_KEY'] = api_key
-    sb_api_key = api_key
-
-def change_huggingface_api_key(api_key):
-    # No default key
-    global hf_api_key
-    hf_api_key = api_key
-
-def change_pinecone_api_key(api_key):
-    # No default key
-    global pc_api_key
-    pc_api_key = api_key
-
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Streamlit View Components
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 def model_select_section():
     global oa_api_key
     global hf_api_key
@@ -239,6 +178,59 @@ def vector_db_select_section():
         else:
             st.write("Supabase API Key Invalid or Unauthorized. Please Try Again.")
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# VectorDB Document Upload and Integration into LLM Pipeline
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+def get_text(pdfs):
+    text = ""
+    for pdf in pdfs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_batches(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size = 1000,
+        chunk_overlap = 100
+    )
+    return text_splitter.split_text(text)
+
+def ingest(pdfs):
+    text = get_text(pdfs)
+    text_batches = get_text_batches(text)
+
+    return text_batches
+
+# Creates Vector DB
+def init_vector_db(text_batches):
+    print(text_batches)
+    embeddings = OpenAIEmbeddings(openai_api_key=oa_api_key)
+    vector_db = SupabaseVectorStore.from_texts(text_batches, embeddings, client=supabase, table_name="documents")
+    
+    # for switching to pinecone
+    # vecter_db = pinecone.create_index("python-index", dimension=1536, metric="cosine")
+
+    return vector_db
+
+def get_conversation_chain(vector_db):
+    if llm == "OpenAI":
+        llm = ChatOpenAI(openai_api_key=oa_api_key, model=llm_model)
+    elif llm == "HuggingFace":
+        llm = HuggingFaceHub(repo_id=llm_model, model_kwargs={"temperature":0.5, "max_length":512})
+
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vector_db.as_retriever(search_type="similarity"),
+        memory=memory
+    )
+
+    return conversation_chain
+
 def document_upload_section():
     pdfs = st.file_uploader("Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
     if st.button("Process"):
@@ -252,9 +244,23 @@ def document_upload_section():
             # create conversation chain
             st.session_state.conversation = get_conversation_chain(vector_db)
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Runner
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+def generate_answer(user_input):
+    response = st.session_state.conversation({'question': user_input})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st_message(message.content, is_user=True)
+        else:
+            st_message(message.content, is_user=False)
+    
+    # Clears the input text
+    # st.session_state["input_text"] = ""
+
 def main():
     # Sidebar Configuration
     global sb_api_key
